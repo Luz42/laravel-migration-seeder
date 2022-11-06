94dcad837246f4e544d566987a52ff7a49a56aa8
Luz42 <a.evangelista93@gmail.com>
2022-11-06 11:38:53 +0100

    Implementata la gestione dei dati in HomeControlle

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Train;
+
 class HomeController extends Controller
 {
     //
+    public function index(){
+        $trains = Train::all();
+        //dd($trains);
+        return view('home', compact('trains'));
+    }
 }
