b65b81e3e4e7451ac90a549ccdb812ec74696419
Luz42 <a.evangelista93@gmail.com>
2022-11-06 11:23:58 +0100

    Implementato il seeder ed eseguito
    inseriti in tabella 100 record

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -16,6 +16,51 @@ class TrainTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         //
+        for($i=0; $i <100; $i++){
+
+            $train = new Train();
+        /*
+            $departureTime = $faker->time();
+            
+            function getHour($time){
+                
+                $splitTime = explode(':', $time);
+
+                return $splitTime[0];
+                
+
+            }
+
+            function getArrivalTime(){
+                
+                $arrivalTime = $faker->time();
+
+                $arrivalHour = getHour($arrivalTime);
+                $departureHour = getHour($this->departureTime);
+
+                if($arrivalHour < $departureHour ){
+
+                    return $departureHour + 1 . ':00:00'; 
+
+                }
+
+            };
+        */            
+            $randomDate = $faker->date();
+
+            $train->azienda = $faker->name();
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->data_partenza = $randomDate;
+            $train->data_arrivo = $randomDate;
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->isbn10();
+            $train->numero_carrozze = $faker->numberBetween(1, 25);
+
+            $train->save();
+        }
+        
 
     }
 }
